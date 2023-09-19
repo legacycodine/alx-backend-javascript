5c1a42cc2e59b591f4e8a8820d04607d4da5e5ff
legacycodine <adebimpee842@gmail.com>
2023-09-19 21:10:43 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
-// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const readline = require('readline');
-
-const readline = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-console.log("Welcome to Holberton School, what is your name?");
-
-readline.question("", (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log("This important software is now closing");
-  readline.close();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
