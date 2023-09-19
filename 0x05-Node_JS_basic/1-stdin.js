556009a84275a130d3031c3a5106e97e00e4c5c3
legacycodine <adebimpee842@gmail.com>
2023-09-19 21:05:00 +0000

    stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+// 1-stdin.js
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("Welcome to Holberton School, what is your name?");
+
+rl.question("", (name) => {
+  console.log(`Your name is: ${name}`);
+  console.log("This important software is now closing");
+  rl.close();
+});
